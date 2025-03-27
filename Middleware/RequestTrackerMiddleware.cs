ae12df6ace9eaf641029496bc93d90f19c388c72
Jean Bryant Figueroa <jbster559@gmail.com>
2025-03-27 16:42:52 -0700

    Add project files.

---
+++ Middleware/RequestTrackerMiddleware.cs
@@ -0,0 +1,21 @@
+﻿namespace COMP003B.Assignment2.Middleware
+{
+{
+    public class RequestLoggingMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public RequestLoggingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            Console.WriteLine($"[Request] {context.Request.Method} {context.Request.Path}");
+            await _next(context);
+            Console.WriteLine($"[Request] {context.Response.StatusCode}");
+        }
+    }
+}
+}
---
+++ Program.cs
@@ -0,0 +1,46 @@
+//Author : Jean Bryant Figueroa
+//Class : COMP003B
+//Fauculty : Johnathan Cruz
+//Purpose : MVC Assignment
+
+namespace COMP003B.Assignment2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseStaticFiles();
+
+            app.UseMiddleware<COMP_003B.Assignmenty2.Middleware.RequestLoggingMiddleware>();
+
+            app.UseWelcomePage("/welcome");
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
