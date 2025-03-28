1deb84cc3ad2b2cb0ba6b32f38bbfb28dd897814
Jean Bryant Figueroa <jbster559@gmail.com>
2025-03-27 18:35:39 -0700

    Updated Current/Ideas/Complete to have needed HTML

---
+++ Views/Shared/_Sidebar.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace COMP003B.Assignment2.Views.Shared
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
